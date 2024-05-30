2a37544a70c66c2505b0d96e5e97d36137283dd5
JaceKong <78537442+jacekong@users.noreply.github.com>
2024-05-30 21:13:42 +0700

    modified settings.py

--- chat/channel_middlware.py
+++ chat/channel_middlware.py
@@ -10,42 +10,6 @@ from jwt.exceptions import InvalidTokenError, ExpiredSignatureError
 from users.models import CustomUser
 
 class JWTAuthMiddleware(BaseMiddleware):
-    # def __init__(self, inner):
-    #     self.inner = inner
-        
-    # async def __call__(self, scope, receive, send):
-    #     close_old_connections()
-        
-    #     # Extract the JWT token from the query parameters or headers
-    #     query_string = scope.get('query_string', b'').decode('utf-8')
-    #     query_parameters = dict(qp.split('=') for qp in query_string.split('&'))
-    #     token = query_parameters.get('token', None)
-        
-    #     if token is None:
-    #         await send({
-    #             'type': 'websocket.close',
-    #             'code': 4000
-    #         })
-            
-    #     authentication = JWTAuthentication()
-        
-    #     try:
-    #         user = await authentication.authenticate_user(scope, token)
-    #         # print(f'------- user {user} --------')
-    #         if user is not None:
-    #             scope['user'] = user
-    #         else:
-    #             await send({
-    #                 'type': 'websocket.close',
-    #                 'code': 4000
-    #             })
-            
-    #         return await super().__call__(scope, receive, send)
-    #     except AuthenticationFailed:
-    #         await send({
-    #             'type': 'websocket.close',
-    #             'code': 4002
-    #         })
     def __init__(self, inner):
         self.inner = inner
             
